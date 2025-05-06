76dcc94a7ba6ce9bf80fd8b8cece7bde2f46cb80
Leonardo Monge <leonardomonge19@gmail.com>
2025-05-05 21:02:20 -0600

    chore(game): add avance 2 exercise

--- game/play.ts
+++ game/play.ts
@@ -16,32 +16,44 @@ interface Pokemon {
     {id: 4, nombre:"Bulbasur", tipo:"Planta", ataque:34 , defensa:26 }
   ]
  
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-function ElegirPokemon(pokemones: Pokemon[] ): void {
-    
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
+  function preguntarpokemon(pregunta: string): Promise<string> {
+    return new Promise((resolve) => {
+      rl.question(pregunta, (respuesta) => {
+        resolve(respuesta);
       });
-
-
-    pokemones.forEach(info =>{
-        console.log("Num#:",info.id, "Nombre:",info.nombre, "Tipo:",info.tipo)
     });
-    rl.question(" Elije el numero de pokemon con el que vas a luchar: ", (respuesta:string) => {
+  }
 
-      const idElegido = parseInt(respuesta);
-      const seleccionado = pokemones.find(p => p.id === idElegido);
-
-     console.log(" El pokemon que elejiste fue: ",  seleccionado?.nombre)
-     rl.close();
+async function ElegirPokemon() {
+  
+    pokemones.forEach(info => {
+      console.log("Num#:", info.id, "Nombre:", info.nombre, "Tipo:", info.tipo)
     });
+ const seleccion1 = await preguntarpokemon(" Indique el primer numero:")
+ const seleccion2 = await preguntarpokemon(" Indique el segundo numero:")
 
+ const pokemon1 = pokemones[parseInt(seleccion1) - 1]
+ const pokemon2 = pokemones[parseInt(seleccion2) - 1]
 
+ console.log( " Jugador 1 seleccionaste a: ", pokemon1.nombre)
+ console.log( " Jugador 2 seleccionaste a: ", pokemon2.nombre)
+
+ rl.close();
 }
 
-ElegirPokemon(pokemones)
-    
-  
 
- 
+ElegirPokemon();
+    
+
+
+ // preguntar : nombre de la funcion
+ // (pregunta: string) : el parametro que recibe, sera  un string " indique el pokemon?"
+ /* Promise<string>  : un objeto que representa un valor disponible cuando termine la 
+ operacion, osea " el pokemon seleccionado es pikachu" pero no lo tengo hasta que yo llame otra 
+ vez la funcion abajo */
+ //return new Promise((resolve) => { almacena la respuesta por un momento, el es mensajero
